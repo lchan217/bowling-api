95c5e8d9829c90ee7ced3cb640ad202c2e32ce04
Lizzie <lchan217@gmail.com>
2020-02-04 14:40:11 -0700

    add skeleton of controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+    def index
+        @games = Game.all
+        render json: @games, status: 201
+    end
+
+    def create
+        game = Game.new
+    end
+
+    def update 
+        game = Game.find(params[:id])
+        render json: game
+    end 
+    
+    def book_params
+        params.require(:game).permit(:pins, :score)
+    end
 end
