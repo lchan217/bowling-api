4564a73e4608b223f8c8e5dbd4efd242687e4388
Lizzie <lchan217@gmail.com>
2020-02-04 20:05:50 -0700

    add gameOver attr

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :pins, :score, :frame, :spareBalls, :strikeBalls, :turn
+  attributes :id, :pins, :score, :frame, :spareBalls, :strikeBalls, :turn,  :game_over
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_024802) do
+ActiveRecord::Schema.define(version: 2020_02_05_030249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_02_05_024802) do
     t.integer "turn"
     t.integer "spareBalls"
     t.integer "strikeBalls"
+    t.boolean "game_over", default: false
   end
 
 end
