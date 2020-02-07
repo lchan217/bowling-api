63d835e977fb96a66d55a532944ec41247467f82
Lizzie <lchan217@gmail.com>
2020-02-07 13:49:28 -0700

    refactor spare calc

--- app/models/concerns/game_concern.rb
+++ app/models/concerns/game_concern.rb
@@ -7,13 +7,6 @@ module GameConcern
                 if game.frame != 10 
                     game.score_hash[previousFrame.to_s] += game.score
                 end
-                #scored a spare in the final round?
-                if game.frame === 10
-                    calculate_total(game)
-                    game.game_over = true 
-                    game.save
-                    exit
-                end 
             end
         end
         def previous_strike(game)
@@ -23,16 +16,6 @@ module GameConcern
                 game.strikeBalls -= 1
                 game.score_hash[previousFrame.to_s] += game.score 
             end
-
-            #scored a strike in the final round?
-            # if game.frame === 11 && game.strikeBalls === 0
-            #     calculate_total(game)
-            #     game.game_over = true 
-            #     game.save
-            #     exit
-            # end 
-
-          
 
             #2 strikes in a row
             if game.score_hash[previouspreviousFrame.to_s] === 20 && spareBalls == 0 && game.turn === 2 && game.score_hash[game.frame.to_s] != 10
@@ -47,18 +30,27 @@ module GameConcern
                 total += score
             end
 
-            #strike scored in final round 1st throw
+            #strike scored in final round, 1st throw
             if game.frame === 11 && game.strikeBalls === 1 
                 total -= game.score_hash[game.frame.to_s]
             end
 
-            #strike scored in final round 2nd throw
+            #strike scored in final round, 2nd throw
             if game.frame === 12 && game.strikeBalls === 0
                 game.total_score += game.score
                 game.game_over = true 
                 game.save
                 exit
             end
+
+            #spare scored  in final round, only throw
+            if game.frame === 10
+                game.total_score += game.score
+                game.game_over = true 
+                game.save
+                exit
+            end 
+
             game.total_score = total
         end
        
